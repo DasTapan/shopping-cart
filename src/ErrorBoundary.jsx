f4fdb0d77e843f9d7f2ee0f6631fd79729b308ae
Tapan Das <tapands.das@gmail.com>
2023-04-06 21:23:14 +0530

    Install react-router-dom

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+    console.log(info);
+  }
+
+  render() {
+    if (this.state.hasError) return <span>Some thing went wrong here</span>;
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
