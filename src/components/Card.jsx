fbf0a7104c994aa498ca9c58ca163903fe030b5c
Tapan Das <tapands.das@gmail.com>
2023-04-07 10:49:12 +0530

    Handle change of item numbers in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import "./styles/Card.scss";
 
-const Counter = ({ value }) => {
+const Counter = ({ value, handleIncrement, handleDecrement }) => {
   return (
     <div className="counter">
-      <div className="decrease">&#8593;</div>
+      <div className="decrease" onClick={handleDecrement}>
+        &#8595;
+      </div>
       <div className="value">{value}</div>
-      <div className="increase">&#8595;</div>
+      <div className="increase" onClick={handleIncrement}>
+        &#8593;
+      </div>
     </div>
   );
 };
 
 const Card = ({ img, name }) => {
   const [quantity, setQuantity] = useState(1);
+
+  const increase = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrease = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
   return (
     <div className="card">
       <div className="img-box">item pic here</div>
       <span>item name</span>
-      <Counter value={quantity} />
+      <Counter
+        value={quantity}
+        handleIncrement={increase}
+        handleDecrement={decrease}
+      />
       <button className="add-to-cart">Add to Cart</button>
     </div>
   );
