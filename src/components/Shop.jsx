041faaf0658893c05247623ad28f9417dc5d53cf
Tapan Das <tapands.das@gmail.com>
2023-04-11 10:29:51 +0530

    Update README

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,15 +1,7 @@
-// import { useEffect } from "react";
 import Card from "./Card";
 import "./styles/Shop.scss";
-// import hunterDreamSound from "./assets/sounds/hunter-dream.mp3";
 
 const Shop = ({ catalog, addToCart }) => {
-  // useEffect(() => {
-  //   const themeSound = new Audio(hunterDreamSound);
-  //   themeSound.volume = 0.5;
-  //   themeSound.play();
-  // }, []);
-
   const listedIItems = catalog.map((item) => (
     <Card
       key={item.id}
